1abf64280c3de51f94fb91e4b808a6375f4c4415
Angelica Hernandez <angelicahernandez@Angelicas-MacBook-Pro.local>
2019-08-04 12:45:41 -0400

    official  start :)

--- swi-app/src/App.js
+++ swi-app/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, FormControl, Form, Nav, Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#profile">Profile</Nav.Link>
+        <Nav.Link href="#companies">Companies</Nav.Link>
+        <Nav.Link href="#events">Events</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Navbar>
   );
-}
+} 
 
 export default App;
