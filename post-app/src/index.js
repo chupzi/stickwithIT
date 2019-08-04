787a88e48a8cf8a6463728aba4f03885f1fb80e1
kiara.rose <kiara.rose@mlb.com>
2019-08-04 10:25:41 -0400

    Added components folder

--- post-app/src/index.js
+++ post-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Post from "./Components/Post";
+import Post from "./Components/Post/Post.jsx";
 
 ReactDOM.render(<Post />, document.getElementById("root"));
 
