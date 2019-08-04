b2537c2278a1f73f9c8487336e4577762f4c821c
kiara.rose <kiara.rose@mlb.com>
2019-08-04 09:37:37 -0400

    Initial setup of post-app

---
+++ post-app/src/Components/Post.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Post = props => {
+  return (
+    <div>
+      <input id="1234" type="submit" value="hello" />
+    </div>
+  );
+};
+Post.propTypes = {};
+Post.defaultProps = {};
+
+export default Post;
