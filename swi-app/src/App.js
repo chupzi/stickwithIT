6ae3f3d40fb9d83b10239427051a292002a1937a
kiara.rose <kiara.rose@mlb.com>
2019-08-04 15:38:24 -0400

    Added Navbar to Components

--- swi-app/src/App.js
+++ swi-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CardModal from './Components/Card/CardModal';
-
+import NavBar from './Components/NavBar/NavBar';
 function App() {
   return (
     <div className="App">
@@ -21,6 +21,7 @@ function App() {
         </a>
       </header> */}
       {/* <div style={{display:'flex', flexWrap: 'wrap'}}> */}
+      <NavBar />
         <div className="hi" style={{ display: 'grid', gridTemplateColumns:'repeat(4, 1fr)'}}>
           <CardModal  /> 
           <CardModal />
