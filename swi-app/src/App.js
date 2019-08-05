a52c8a48830906cdd2d248cb4ae2928b60c9c3b1
Angelica Hernandez <angelicahernandez@Angelicas-MacBook-Pro.local>
2019-08-05 09:23:34 -0400

    i dont remember what i did

--- swi-app/src/App.js
+++ swi-app/src/App.js
@@ -3,30 +3,33 @@ import './App.css';
 import CardModal from './Components/Card/CardModal';
 import Box from './Components/Box/Box';
 import Media from './Components/Media/MediaHolder';
+import Profile from './pages/Profile';
 
 import NavBar from './Components/NavBar/NavBar';
 import Post from './Components/Post/Post';
-import {Nav, Navbar} from 'react-bootstrap';
-import {Route, HashRouter, NavLink} from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
+import { Route, HashRouter, NavLink } from 'react-router-dom';
 function App() {
   return (
     <HashRouter>
       <div className="App">
-          <Navbar bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark">
           <Navbar.Brand href="#home">StickWithIT</Navbar.Brand>
           <Nav>
             <ul className="header">
               <li className="list"><NavLink className="link" to="/">Home</NavLink></li>
               <li className="list"><NavLink className="link" to="/stuff">Stuff</NavLink></li>
               <li className="list"><NavLink className="link" to="/contact">Contact</NavLink></li>
+              <li className="list"><NavLink className="link" to="/profile">Profile</NavLink></li>
             </ul>
           </Nav>
-          </Navbar>
-            <div className="content">
-              <Route exact path="/" component={CardModal}/>
-              <Route path="/stuff" component={Post}/>
-              <Route path='/contact' component={Post} />
-            </div>
+        </Navbar>
+        <div className="content">
+          <Route exact path="/" component={CardModal} />
+          <Route path="/stuff" component={Post} />
+          <Route path='/contact' component={Post} />
+          <Route path='/profile' component={Profile} />
+        </div>
       </div>
     </HashRouter>
   );
